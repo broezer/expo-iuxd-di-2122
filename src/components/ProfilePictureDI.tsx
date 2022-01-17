0d1ecc83e9b9bc1aae42da49f10c79bbd807c2e0
broezer <bruce@moerdjiman.com>
2022-01-17 21:43:24 +0100

    fix to lowercase

--- src/components/ProfilePictureDI.tsx
+++ src/components/ProfilePictureDI.tsx
@@ -4,9 +4,9 @@ const ProfilePictureDI: FC<{ src: string }> = ({ src }) => {
   const [isLoaded, setIsLoaded] = useState<boolean>(false);
 
   const imgRef = useRef(null);
+  let imageSrc = src.toLowerCase();
   useEffect(() => {
-    var src = src.toLowerCase();
-    imgRef.current.src = '/di/profiles/' + src + '.jpg';
+    imgRef.current.src = '/di/profiles/' + imageSrc + '.jpg';
 
     imgRef.current.addEventListener('loadeddata', () => {
       setIsLoaded(true);
